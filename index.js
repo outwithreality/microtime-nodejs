64bbd4151f21a88b93b18114a32ff20e4356a55d
David Alexander <david@cpringsmith.com>
2015-09-22 08:42:49 +0100

    Add an 'always increment' pico-second method.

--- index.js
+++ index.js
@@ -11,11 +11,13 @@
 	starttime=Math.trunc(acctime);
 	now = process.hrtime();
     }
-    startsec=starttime-now[0];
-    startsub=Math.trunc(now[1]/1000);
+    var start_secs=starttime-now[0];
+    var start_usecs=Math.trunc(now[1]/1000);
+    var start_psecs=Math.trunc(now[1]);
     
     var last_secs=0;
     var last_usecs=0;
+    var last_psecs=0;
 
     /* module exports */
     module.exports = {
@@ -24,14 +26,14 @@
 	    return nowTime[0]*1000000+nowTime[1];
 	},
 	nowDouble: function(){
-	    var nowTime = module.exports.nowStruct(); 
-	    return nowTime[0]+nowTime[1]/1000000;
+	    var nowTime = module.exports.nowStructPicoIncr(); 
+	    return nowTime[0]+nowTime[1]/1000000000;
 	},
 	nowStruct: function(){
 	    var now = process.hrtime();
 	    
-	    var now_secs = Math.trunc(now[0]+startsec);
-	    var now_usecs = Math.trunc(now[1]/1000)-startsub;
+	    var now_secs = Math.trunc(now[0]+start_secs);
+	    var now_usecs = Math.trunc(now[1]/1000)-start_usecs;
 	    if (now_usecs < 0) {
 		now_usecs=1000000+now_usecs;
 		now_secs--;
@@ -43,14 +45,14 @@
 	nowStructIncr: function(){
 	    var now = process.hrtime();
 	    
-	    var now_secs = Math.trunc(now[0]+startsec);
-	    var now_usecs = Math.trunc(now[1]/1000)-startsub;
+	    var now_secs = Math.trunc(now[0]+start_secs);
+	    var now_usecs = Math.trunc(now[1]/1000)-start_usecs;
 	    if (now_usecs < 0) {
 		now_usecs=1000000+now_usecs;
 		now_secs--;
 	    }
 	    if ((now_secs<last_secs) || 
-		((now_secs==last_secs) && (now_usecs<=last_usecs))
+		( (now_usecs<=last_usecs) &&(now_secs==last_secs))
 	       )
 	    {
 		now_usecs=last_usecs+1;
@@ -64,6 +66,31 @@
 	    last_usecs=now_usecs;
 	    last_secs=now_secs;
 	    return [now_secs, now_usecs];
+	},
+	nowStructPicoIncr: function(){
+	    var now = process.hrtime();
+	    
+	    var now_secs = Math.trunc(now[0]+start_secs);
+	    var now_psecs = now[1]-start_psecs;
+	    if (now_psecs < 0) {
+		now_psecs=1000000000+now_psecs;
+		now_secs--;
+	    }
+	    if ((now_secs<last_secs) ||
+		((now_psecs<=last_psecs) && (now_secs==last_secs))
+	       )
+	    {
+		now_psecs=last_psecs+1;
+		now_secs=last_secs;
+		if (now_usecs===1000000000)
+		{
+		    now_psecs=now_psecs-1000000000;
+		    now_secs=last_secs+1;
+		}
+	    }
+	    last_psecs=now_psecs;
+	    last_secs=now_secs;
+	    return [now_secs, now_psecs];
 	}
     };
 }).call(this);
