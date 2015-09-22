0028a3cce13990cf0f0580b4a0fc3ffb20938216
David Alexander <david@cpringsmith.com>
2015-09-22 03:04:58 +0100

    Use milli-seconds rather than nano; Add an 'always increment' method.

--- index.js
+++ index.js
@@ -12,28 +12,58 @@
 	now = process.hrtime();
     }
     startsec=starttime-now[0];
-    startsub=now[1];
+    startsub=Math.trunc(now[1]/1000);
     
+    var last_secs=0;
+    var last_usecs=0;
+
     /* module exports */
     module.exports = {
 	now: function(){
-	    var nowTime = module.exports.nowStruct(); 
-	    return nowTime[0]*1000000000+nowTime[1];
+	    var nowTime = module.exports.nowStruct();
+	    return nowTime[0]*1000000+nowTime[1];
 	},
 	nowDouble: function(){
 	    var nowTime = module.exports.nowStruct(); 
-	    return nowTime[0]+nowTime[1]/1000000000;
+	    return nowTime[0]+nowTime[1]/1000000;
 	},
 	nowStruct: function(){
 	    var now = process.hrtime();
 	    
-	    var now_secs = now[0]+startsec;
-	    var now_usecs = now[1]-startsub;
-	    if (now_usecs<0) {
-		now_usecs= 1000000000+now_usecs;
+	    var now_secs = Math.trunc(now[0]+startsec);
+	    var now_usecs = Math.trunc(now[1]/1000)-startsub;
+	    if (now_usecs < 0) {
+		now_usecs=1000000+now_usecs;
 		now_secs--;
 	    }
-	    return [Math.trunc(now_secs), Math.trunc(now_usecs)];
+	    last_usecs=now_usecs;
+	    last_secs=now_secs;
+	    return [now_secs, now_usecs];
+	},
+	nowStructIncr: function(){
+	    var now = process.hrtime();
+	    
+	    var now_secs = Math.trunc(now[0]+startsec);
+	    var now_usecs = Math.trunc(now[1]/1000)-startsub;
+	    if (now_usecs < 0) {
+		now_usecs=1000000+now_usecs;
+		now_secs--;
+	    }
+	    if ((now_secs<last_secs) || 
+		((now_secs==last_secs) && (now_usecs<=last_usecs))
+	       )
+	    {
+		now_usecs=last_usecs+1;
+		now_secs=last_secs;
+		if (now_usecs===1000000)
+		{
+		    now_usecs=now_usecs-1000000;
+		    now_secs=last_secs+1;
+		}
+	    }
+	    last_usecs=now_usecs;
+	    last_secs=now_secs;
+	    return [now_secs, now_usecs];
 	}
     };
 }).call(this);
--- test.js
+++ test.js
@@ -1,19 +1,42 @@
 var microtime = require('./index.js')
 var x;
-var old=0;
+var old=[0,0];
 for (x=0; x<10000000; x++)
 {
     var y=microtime.now();
     if (y<=old)
     {
-	console.log("BALDERDASH");
+//	console.log("BALDERDASH  "+old+"  "+y);
+    }
+    else 
+    {
+//	console.log("BINGO  "+y+"  "+old);
     }
     old=y;
 }
+for (x=0; x<10000000; x++)
+{
+    var y=microtime.nowStructIncr();
+    if ((y[0]<old[0]) || ((y[0]===old[0]) && (y[1]<=old[1])))
+    {
+	console.log("BALDERDASH  "+old[0]+":"+old[1]+" <= "+y[0]+":"+y[1]);
+    }
+    else 
+    {
+//	console.log("BINGO  "+y+"  "+old);
+    }
+    old=y;
+}
+
+
 console.log(microtime.now());
 //1297448895297028
+//1442882359293026300
 
 console.log(microtime.nowDouble());
 //1297448897.600976
+//1442882359.2960062
 console.log(microtime.nowStruct());
 //[ 1297448902, 753875 ]
+//[ 1442882359, 296780831 ]
+
